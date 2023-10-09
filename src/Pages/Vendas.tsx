f43c480807264fbce55540c1547ebd5557ead1fa
unknown <rafazel@outlook.com>
2023-10-09 14:06:48 -0300

    First commit

---
+++ src/Pages/Vendas.tsx
@@ -0,0 +1,20 @@
+import { useData } from "../Context/DataContext";
+import VendaItem from "../Components/VendaItem";
+
+const Vendas = () => {
+  const { data } = useData();
+
+  if (!data) return;
+
+  return (
+    <ul>
+      {data.map((venda) => (
+        <li key={venda.id}>
+          <VendaItem venda={venda} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Vendas;
